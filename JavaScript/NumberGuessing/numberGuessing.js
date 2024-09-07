82beda4b04ac16f99a94a1a14021a372d72b1175
syddgrt <syedyangsebenar@gmail.com>
2024-09-07 13:32:09 +0800

    commit of kydpprojects folder

--- JavaScript/NumberGuessing/numberGuessing.js
+++ JavaScript/NumberGuessing/numberGuessing.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')({sigint: true});
  
 // Random number from 1 - 100
-let numberInMind = Math.floor(Math.random() * 10) + 1;
+let numberInMind = Math.floor(Math.random() * 100) + 1;
 
 // This variable is used to determine if the app should continue prompting the user for input
 let foundCorrectNumber = false;
